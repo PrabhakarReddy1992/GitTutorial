8fc93641985ff1240ae76d0ed16ac53809dffbf2
PrabhakarReddy1992 <prabhakar.madireddy@gmail.com>
2020-09-03 17:00:08 +0530

    for loop commit

--- src/main/java/com/Hello.java
+++ src/main/java/com/Hello.java
@@ -1,9 +1,17 @@
 package com;
 
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List<String> namesList=Arrays.asList("Prabha","Raji");
+		for(int i=0;i<namesList.size();i++ ) {
+			System.out.println(namesList.get(i));
+		}
 
 	}
 
